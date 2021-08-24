ee237459d7313f6ecf89f3fd8768921b1e8a2a82
j-albr16 <65612826+j-albr16@users.noreply.github.com>
2021-08-24 23:42:48 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 
 echo 'Initialize Dotfile Configuration'
 
-git clone https://github.com/j-albr16/dotfiles.git ~/dotfiles && bash ~/dotfiles/symlink.sh
+git clone https://github.com/j-albr16/dotfiles.git ~/dotfiles && cd dotfiles && bash symlink.sh
